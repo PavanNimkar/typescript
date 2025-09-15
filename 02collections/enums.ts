2c92cf948c4fc71170aafe173730d0013faf7dd0
PavanNimkar <pavannnimkar@gmail.com>
2025-09-15 20:24:31 +0530

    enums done

---
+++ 02collections/enums.ts
@@ -0,0 +1,18 @@
+// enum SeatChoice {
+//     AISLE = "aisle",
+//     MIDDLE = "middle",
+//     WINDOW = "window",
+//     NO_PREFERENCE = "no_preference"
+// }
+
+// const mySeat: SeatChoice = SeatChoice.WINDOW
+
+// less javaScript output with using cnonst enum
+const enum SeatChoice {
+    AISLE = "aisle",
+    MIDDLE = "middle",
+    WINDOW = "window",
+    NO_PREFERENCE = "no_preference"
+}
+
+const mySeat: SeatChoice = SeatChoice.WINDOW
